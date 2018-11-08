8a35ea590e2bacd40ded6780e9bf5a27f860f2ec
Jedoi <eag127@ymail.com>
2018-11-08 11:57:43 -0800

    Update Checkers/Assets/Scripts/Timer.cs

--- Checkers/Assets/Scripts/Timer.cs
+++ Checkers/Assets/Scripts/Timer.cs
@@ -1,32 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Timer : MonoBehaviour {
+//public class Timer : MonoBehaviour {
 
-    public int duration = 60;
-    public int timeRemaining;
-    public bool isCountingDown = false;
-    public void Begin()
-    {
-        if (!isCountingDown)
-        {
-            isCountingDown = true;
-            timeRemaining = duration;
-            Invoke("_tick", 1f);
-        }
-    }
+  //  public int duration = 60;
+ //   public int timeRemaining;
+   // public bool isCountingDown = false;
+   // public void Begin()
+   // {
+     //   if (!isCountingDown)
+    //    {
+      //      isCountingDown = true;
+       //     timeRemaining = duration;
+       //     Invoke("_tick", 1f);
+    //    }
+   // }
 
-    private int _tick()
-    {
-        timeRemaining--;
-        if (timeRemaining > 0)
-        {
-            Invoke("_tick", 1f);
-        }
-        else
-        {
-            isCountingDown = false;
-        }
-    }
-}
+   // private int _tick()
+   // {
+     //   timeRemaining--;
+      //  if (timeRemaining > 0)
+      //  {
+      //      Invoke("_tick", 1f);
+      //  }
+       // else
+       // {
+        //    isCountingDown = false;
+       // }
+   // }
+//}
